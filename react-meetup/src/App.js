c6706fb0eee299df44e3cc31bb684cddb820ea4c
Joris Jansen <joris97jansen@gmail.com>
2021-05-15 15:28:32 +0200

    React Meetup project

---
+++ react-meetup/src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup" exact>
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites" exact>
+          <Favorites />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
